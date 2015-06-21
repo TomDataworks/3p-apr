b4164be5789c341316990a9fc7ae9d05a82155d6
Drake Arconis <drake@alchemyviewer.org>
2015-06-21 05:19:54 -0400

    Cleanup and fix a few small build script bugs

--- build-cmd.sh
+++ build-cmd.sh
@@ -130,29 +130,26 @@ case "$AUTOBUILD_PLATFORM" in
     opts="${TARGET_OPTS:--arch i386 -arch x86_64 -iwithsysroot $sdk -mmacosx-version-min=10.8}"
 
     pushd "$TOP_DIR/apr"
-    CC="clang" CFLAGS="$opts" CXXFLAGS="$opts" LDFLAGS="$opts" \
-        ./configure --prefix="$PREFIX"
-    make
-    make install
+        CC="clang" CFLAGS="$opts" CXXFLAGS="$opts" LDFLAGS="$opts" \
+            ./configure --prefix="$PREFIX"
+        make
+        make install
 
-    if [ "${DISABLE_UNIT_TESTS:-1}" = "0" ]; then
-        make check
-    fi
-
-
+        if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+           make check
+        fi
     popd
 
     pushd "$TOP_DIR/apr-util"
-    CC="clang" CFLAGS="$opts" CXXFLAGS="$opts" LDFLAGS="$opts" \
-        ./configure --prefix="$PREFIX" --with-apr="$PREFIX" \
-        --with-expat="$PREFIX"
-    make
-    make install
+        CC="clang" CFLAGS="$opts" CXXFLAGS="$opts" LDFLAGS="$opts" \
+            ./configure --prefix="$PREFIX" --with-apr="$PREFIX" \
+            --with-expat="$PREFIX"
+        make
+        make install
 
-    if [ "${DISABLE_UNIT_TESTS:-1}" = "0" ]; then
-        make check
-    fi
-
+       if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+           make check
+       fi
     popd
 
     # To conform with autobuild install-package conventions, we want to move
